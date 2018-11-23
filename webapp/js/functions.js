f7b95406923d498a270bdbe795fa7c072f55dcb5
jrneryr <juniorneryr@gmail.com>
2018-11-23 16:25:19 -0200

    inclusão do html e funçoes js

---
+++ webapp/js/functions.js
@@ -0,0 +1,15 @@
+function mudaFoto1(foto) {
+    document.getElementById("prod1").src= foto;
+}
+
+function mudaFoto2(foto) {
+    document.getElementById("prod2").src= foto;
+}
+
+function mudaFoto3(foto) {
+    document.getElementById("prod3").src= foto;
+}
+
+function mudaFoto4(foto) {
+    document.getElementById("prod4").src= foto;
+}
